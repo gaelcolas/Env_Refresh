c85d1e8df952face01f1ca956871c2fa248a25d8
gaelcolas <gaelcolas@users.noreply.github.com>
2016-07-18 15:47:09 +0100

    separating manual run and dsc config, removing configData

---
+++ examples/Execute_defaultConfig_manually.ps1
@@ -0,0 +1,5 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+. $here\dsc_configuration.ps1
+
+Default 
+Start-DscConfiguration -wait -Force Default -Verbose
--- examples/dsc_configuration.ps1
+++ examples/dsc_configuration.ps1
@@ -8,23 +8,6 @@
             RunName           = 'RMQ_BASE_VAR'
             VariableNameList  = 'RABBITMQ_BASE'
         }
-        Script Test 
-        {
-            SetScript = { 'test' | Write-verbose }
-            GetScript = { return @{result='test'} }
-            TestScript = { return $false }
-        }
     }
     
 }
-
-$configData = @{
-AllNodes = @(
-        @{
-            NodeName = 'localhost';
-        }
-    )
-}
-
-Default 
-Start-DscConfiguration -wait -Force Default -Verbose
